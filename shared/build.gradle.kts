15e7c4ee586bbd9eac207d3ceb1c40da9feb6db0
konradjurkowski <konrad.j@thehouseofcode.com>
2025-04-24 15:59:45 +0200

    version 0.0.3

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -61,7 +61,7 @@ mavenPublishing {
     coordinates(
         groupId = "io.github.konradjurkowski",
         artifactId = "snackbarkmm",
-        version = "0.0.2"
+        version = "0.0.3"
     )
 
     pom {
